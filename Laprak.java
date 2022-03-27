801a3cb4d435fde58307fc15e92414c0b00e6197
hilmy771 <102466948+hilmy771@users.noreply.github.com>
2022-03-27 23:51:11 +0700

    Update Laprak.java

--- Laprak.java
+++ Laprak.java
@@ -1,2 +1,19 @@
 public class Laprak {
+  package Tugas;
+import java.util.Scanner;
+public class Laprak {
+    public static void pembukaan(){
+        System.out.println("TUGAS MODUL 4 Kelompok 11");
+        System.out.println("");
+    }
+    public static void main(String[] args) {
+        Scanner dataIn = new Scanner(System.in);
+        int jml_pesanan = 1;
+        int harga = 0;
+        String kamar = null;
+        //funtion
+        pembukaan();
+        //pemanggil objek
+        method object = new method();
+        object.menu();
 }
