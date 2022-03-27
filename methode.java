b1b9af7ca2a8cb401c8de988f532563125a3a9f4
hilmyulhaq <102451371+hilmyulhaq@users.noreply.github.com>
2022-03-27 23:27:36 +0700

    Update methode.java

--- methode.java
+++ methode.java
@@ -1,2 +1,20 @@
 public class methode {
+    public void menu(){
+        //Menu
+        System.out.println("-----------------------------------");
+        System.out.println("      HOTEL KRUSONG LUI VITONG     ");
+        System.out.println("-----------------------------------");
+        System.out.println("          ...Tipe Room...          ");
+        System.out.println("1. Single Room [S]");
+        System.out.println("2. Double Room [D]");
+        System.out.println("3. Triple Room [T]");
+        System.out.println("4. Deluxe Room [DL]");
+        System.out.println("5. Presidential Room [P]");
+        System.out.println("-----------------------------------");
+
+    }
+    public String penutup (String name){
+        return "Terimakasih " + name + " Atas Kebijaksanaan anda untuk memesan hotel kami yang luar biasa tidak adanya";
+    }
+}
 }
