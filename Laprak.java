7604ee367e98e100b5c707ad8889fe95870f625f
aditakbars <102457010+aditakbars@users.noreply.github.com>
2022-03-28 00:06:35 +0700

    Update Laprak.java
    lanjutin

--- Laprak.java
+++ Laprak.java
@@ -11,9 +11,49 @@ public class Laprak {
         int jml_pesanan = 1;
         int harga = 0;
         String kamar = null;
-        //funtion
+        //function
         pembukaan();
         //pemanggil objek
         method object = new method();
         object.menu();
+        //Perulangan While
+        while (jml_pesanan < 3) { //awal perulangan while
+        System.out.println("# pesanan ");
+        //inputan
+        System.out.println("Input Data Ke- " + jml_pesanan);
+        jml_pesanan++;
+        System.out.print("Nama      : ");
+        String nama = dataIn.next();
+        System.out.print("Alamat    : ");
+        String alamat = dataIn.next();
+        System.out.print("Lama Inap (hari): " );
+        int lama = dataIn.nextInt();
+        System.out.print("Tipe Room (KAPITAL) : ");
+        String tipe = dataIn.next();
+
+        switch (tipe) {
+           case "S":
+                harga = 50000;
+                kamar = "Single Room";
+                break;
+           case "D":
+                harga = 100000;
+                kamar = "Double Room";
+                break;
+           case "T":
+                harga = 200000;
+                kamar = "Triple Room";
+                break;
+           case "DL":
+                harga = 150000;
+                kamar = "Deluxe Room";
+                break;
+           case "P":
+                harga = 250000;
+                kamar = "Presidential Room";
+                break;
+          }
+          
+          
+      
 }
