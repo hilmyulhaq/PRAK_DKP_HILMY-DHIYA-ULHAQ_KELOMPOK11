525fd7fc0c2ed5713902362425ca796ea1dfacec
aditakbars <102457010+aditakbars@users.noreply.github.com>
2022-03-27 20:44:02 +0700

    Update methodde1.java
    nambahin gambaran simpel

--- methodde1.java
+++ methodde1.java
@@ -9,6 +9,15 @@ public class bookingHotel{
         /*Program*/
         System.out.println("SELAMAT DATANG DI BOOKING HOTEL KAMI");
         System.out.println("Disini Kami Menyediakan Berbagai Jenis Kamar");
+        
+        
+        System.out.print("berapa malam? = ");
+        int malam = in.nextInt();
+        System.out.print("tipe kamar berapa? = ");
+        int tipe = in.nextInt();
+        int harga = hitung(malam,tipe);
+        System.out.println("harganyaa "+harga+" ribu");
+        
     }
         public static int hitung(int malam, int tipe) {
         int total = malam*tipe;
